b3435bb8b7266dd365d71acadcc974a01f045cd0
David Allan <davidallan@Davids-MacBook-Pro.local>
2023-10-03 21:46:12 +0200

    Chatwindow now setup with javascript to keep it from refreshing page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import openai
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -18,11 +18,40 @@ add_message("system", "You are a helpful assistant.")
 @app.route("/", methods=["GET", "POST"])
 def index():
     global messages
-    global uploaded_file_path  # Make sure to access the global variable
-    upload_confirmation = None  # Variable to store the upload confirmation message
 
     if request.method == "POST":
-        if 'query' in request.form:
+        if request.is_json:
+            # Handle the AJAX request here
+            query = request.json['query']
+            add_message("user", query)  # Keep this line
+
+            try:
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=messages,
+                    temperature=0,
+                    max_tokens=1024,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                )
+
+                # Fetch the role and content of the response message
+                role = response['choices'][0]['message']['role']
+                content = response['choices'][0]['message']['content']
+                add_message(role, content)
+
+                # Return the response to the frontend
+                return jsonify({"role": role, "message": content})
+
+            except Exception as e:
+                print(f"Error: {e}")
+                content = "Sorry, I couldn't process that request."
+                add_message("assistant", content)
+                return jsonify({"role": "assistant", "message": content})
+
+        # This part will handle non-ajax POST requests
+        elif 'query' in request.form:
             query = request.form['query']
             add_message("user", query)  # Keep this line
 
@@ -48,4 +77,4 @@ def index():
                 add_message("assistant", content)
 
 
-    return render_template("index.html", messages=messages, upload_confirmation=upload_confirmation)
+    return render_template("index.html", messages=messages)
